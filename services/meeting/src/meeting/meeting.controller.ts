febc3f4c4e6ce53211ed966627d5b80e1bb560ef
KNP-kanokphon <kanokphonk@osd.co.th>
2022-10-17 16:36:01 +0700

    update snank

--- services/meeting/src/meeting/meeting.controller.ts
+++ services/meeting/src/meeting/meeting.controller.ts
@@ -7,10 +7,13 @@ import {
   Param,
   Delete,
   UseFilters,
+  UseInterceptors,
+  UploadedFile,
   Put,
 } from '@nestjs/common';
 import { MeetingService } from './meeting.service';
 import { AppErrorExceptionFilter } from '@d-debt/share';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { Prisma } from '@prisma/client';
 
 @Controller('meeting')
@@ -29,5 +32,23 @@ export class MeetingController {
   @Post()
   create(@Body() data: Prisma.meetingCreateManyInput) {
     return this.meetingService.create(data);
+  }
+  @Post('import')
+  @UseInterceptors(FileInterceptor('file'))
+  async upload(@UploadedFile() file: Express.Multer.File): Promise<any> {
+    console.log(file);
+    fs.createWriteStream(path).write(buffer);
+    // const foo = papa.parse(file.buffer.toString('utf-8'), {
+    //   delimiter: ',',
+    //   header: true,
+    //   skipEmptyLines: true,
+    // }) as any;
+    // const dataCsv = await csv().fromString(file.buffer.toString('utf-8'));
+    // // console.log(dataCsv);
+    // const result = await this.DebtService.importWrapup(
+    //   dataCsv,
+    //   foo.meta.fields,
+    // );
+    // return { result };
   }
 }
