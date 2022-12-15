0dd1e65c056001fd083f9973a0691afe918dd697
SURADACHK <suradachk@osd.co.th>
2022-12-15 11:56:31 +0700

    update service

--- services/meeting/src/configs/index.ts
+++ services/meeting/src/configs/index.ts
@@ -1,5 +1,5 @@
 export default () => ({
   env: process.env.NODE_ENV ?? 'local',
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 4000,
   id24TokenPublicKey: process.env.ID24_TOKEN_PUBLIC_KEY,
 });
--- services/meeting/src/main.ts
+++ services/meeting/src/main.ts
@@ -1,11 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { PrismaService } from '@d-debt/share';
 import helmet from 'helmet';
-
+import config from './configs';
 import { CoreModule } from './app.module';
-// import { DebtService } from './debt/debt.service';
+
+const PORT = config().port;
 
 async function bootstrap() {
   (BigInt.prototype as any).toJSON = function () {
@@ -14,17 +14,10 @@ async function bootstrap() {
   const app = await NestFactory.create(CoreModule);
   app.enableCors();
   app.use(helmet());
-  app.useGlobalPipes(new ValidationPipe());
-
-  const configService = app.get(ConfigService);
   const prismaService = app.get(PrismaService);
   prismaService.enableShutdownHooks(app);
-  // const debtService = app.get(DebtService);
-
-  // void debtService.setExpire();
-  app.listen(4000);
-  // await app.listen(configService.get('port'), () => {
-  //   console.log(`service start on port ${configService.get('port')}...`);
-  // });
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(PORT);
+  console.log(`service start on port : ${PORT}`);
 }
 bootstrap();
